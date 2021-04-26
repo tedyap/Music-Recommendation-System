13369f47000ca629f91a5f3ef0b2a9ee3a4e78bd
shaankhosla <skhoslajr@gmail.com>
2021-04-26 13:09:08 -0400

    added shell setup and main

---
+++ main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''Starter Pyspark Script for students to complete for their Lab 3 Assignment.
+Usage:
+    $ spark-submit main.py <student_netID>
+'''
+#Use getpass to obtain user netID
+import getpass
+
+# And pyspark.sql to get the spark session
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import avg, min, count, desc, countDistinct, asc
+
+
+def main(spark, netID):
+    '''Main routine for Final Project
+    Parameters
+    ----------
+    spark : SparkSession object
+    netID : string, netID of student to find files in HDFS
+    '''
+    print('Final Project loading ')
+
+
+
+# Only enter this block if we're in main
+if __name__ == "__main__":
+
+    # Create the spark session object
+    spark = SparkSession.builder.appName('part1').getOrCreate()
+
+    # Get user netID from the command line
+    netID = getpass.getuser()
+
+    # Call our main routine
+    main(spark, netID)
