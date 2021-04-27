bea4f12074658f409226402c184bf0989c140c4a
Ted Yap <tyy231@nyu.edu>
2021-04-26 21:03:25 -0400

    initial string indexer

---
+++ data_prep.py
@@ -0,0 +1,32 @@
+from pyspark.sql import SparkSession
+from pyspark.ml.feature import StringIndexer
+from main import get_data
+
+def main(spark):
+    partitions = 1000
+
+    train = get_data(spark, 'cf_train', .01)
+    val = get_data(spark, 'cf_val', .01)
+    test = get_data(spark, 'cf_test', .01)
+
+    for column in ['user', 'track']:
+        indexer = StringIndexer(inputCol=f'{column}_id', outputCol=f'{column}_idx', handleInvalid='keep')
+        indexed = indexer.fit(train)
+        train = indexed.tranform(train)
+        val = indexed.tranform(val)
+        test = indexed.tranform(test)
+        indexer.save(f'{column}_indexer')
+
+    train = train.repartition(partitions, 'user_id').select(['user_idx', 'count', 'track_idx'])
+    train.write.parquet(path='processed_data/cf_train_idx.parquet', mode='overwrite')
+
+    val = val.repartition(partitions, 'user_id').select(['user_idx', 'count', 'track_idx'])
+    val.write.parquet(path='processed_data/cf_validation_idx.parquet', mode='overwrite')
+
+    test = test.repartition(partitions, 'user_id').select(['user_idx', 'count', 'track_idx'])
+    test.write.parquet(path='processed_data/cf_test_idx.parquet', mode='overwrite')
+
+
+if __name__ == "__main__":
+    spark = SparkSession.builder.appName('data_prep').getOrCreate()
+    main(spark)
