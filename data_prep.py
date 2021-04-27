a059d1c81cc9b270a0c8460330580f7a39fa6cd7
Ted Yap <tyy231@nyu.edu>
2021-04-26 22:03:52 -0400

    overwrite save

--- data_prep.py
+++ data_prep.py
@@ -32,21 +32,20 @@ def main(spark):
 
     print('Finished indexing...')
 
-    # train = train.repartition(partitions, 'user_id').select(['user_idx', 'count', 'track_idx'])
-    # assert train_len == train.count()
-    # train.write.parquet(path='processed_data/cf_train_idx.parquet', mode='overwrite')
-    # train.unpersist()
-    #
-    # val = val.repartition(partitions, 'user_id').select(['user_idx', 'count', 'track_idx'])
-    # assert val_len == val.count()
-    # val.write.parquet(path='processed_data/cf_validation_idx.parquet', mode='overwrite')
-    # val.unpersist()
-    #
-    # test = test.repartition(partitions, 'user_id').select(['user_idx', 'count', 'track_idx'])
-    # assert test_len == test.count()
-    # test.write.parquet(path='processed_data/cf_test_idx.parquet', mode='overwrite')
-    # test.unpersist()
+    train = train.repartition(partitions, 'user_id').select(['user_idx', 'count', 'track_idx'])
+    assert train_len == train.count()
+    train.write.parquet(path='processed_data/cf_train_idx.parquet', mode='overwrite')
+    train.unpersist()
 
+    val = val.repartition(partitions, 'user_id').select(['user_idx', 'count', 'track_idx'])
+    assert val_len == val.count()
+    val.write.parquet(path='processed_data/cf_validation_idx.parquet', mode='overwrite')
+    val.unpersist()
+
+    test = test.repartition(partitions, 'user_id').select(['user_idx', 'count', 'track_idx'])
+    assert test_len == test.count()
+    test.write.parquet(path='processed_data/cf_test_idx.parquet', mode='overwrite')
+    test.unpersist()
 
 
 if __name__ == "__main__":
