20b71a3e1472901c1ce5d77f3cc767dc7ced70e7
Ted Yap <tyy231@nyu.edu>
2021-04-26 22:01:49 -0400

    overwrite save

--- data_prep.py
+++ data_prep.py
@@ -6,40 +6,31 @@ Usage:
 
 from pyspark.sql import SparkSession
 from pyspark.ml.feature import StringIndexer
-from pyspark.ml import Pipeline
 from main import get_data
 
 
 def main(spark):
     partitions = 1000
 
-    train = spark.read.parquet('hdfs:/user/bm106/pub/MSD/cf_train.parquet')
-    val = spark.read.parquet('hdfs:/user/bm106/pub/MSD/cf_train.parquet')
-    test = spark.read.parquet('hdfs:/user/bm106/pub/MSD/cf_train.parquet')
+    train = get_data(spark, 'cf_train', .01)
+    val = get_data(spark, 'cf_validation', .01)
+    test = get_data(spark, 'cf_test', .01)
 
-    user_indexer = StringIndexer(inputCol="user_id", outputCol="user_idx", handleInvalid="keep")
-    track_indexer = StringIndexer(inputCol="track_id", outputCol="track_idx", handleInvalid="keep")
+    train_len = train.count()
+    val_len = val.count()
+    test_len = test.count()
 
-    pipeline = Pipeline(stages=[user_indexer, track_indexer])
-    mapping = pipeline.fit(train)
-    train = mapping.transform(train)
+    print('Begin indexing...')
 
+    for column in ['user', 'track']:
+        indexer = StringIndexer(inputCol=f'{column}_id', outputCol=f'{column}_idx', handleInvalid='keep')
+        indexed = indexer.fit(train)
+        train = indexed.transform(train)
+        val = indexed.transform(val)
+        test = indexed.transform(test)
+        indexer.write().overwrite().save(f'{column}_indexer')
 
-    # train_len = train.count()
-    # val_len = val.count()
-    # test_len = test.count()
-    #
-    # print('Begin indexing...')
-    #
-    # for column in ['user', 'track']:
-    #     indexer = StringIndexer(inputCol=f'{column}_id', outputCol=f'{column}_idx', handleInvalid='keep')
-    #     indexed = indexer.fit(train)
-    #     train = indexed.transform(train)
-    #     val = indexed.transform(val)
-    #     test = indexed.transform(test)
-    #     indexer.write().overwrite().save(f'{column}_indexer')
-    #
-    # print('Finished indexing...')
+    print('Finished indexing...')
 
     # train = train.repartition(partitions, 'user_id').select(['user_idx', 'count', 'track_idx'])
     # assert train_len == train.count()
@@ -55,6 +46,7 @@ def main(spark):
     # assert test_len == test.count()
     # test.write.parquet(path='processed_data/cf_test_idx.parquet', mode='overwrite')
     # test.unpersist()
+
 
 
 if __name__ == "__main__":
