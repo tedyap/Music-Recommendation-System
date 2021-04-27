e25d4474f31e12512b5c634549be43a1ffebb752
Ted Yap <tyy231@nyu.edu>
2021-04-26 21:20:18 -0400

    count df

--- data_prep.py
+++ data_prep.py
@@ -9,12 +9,12 @@ def main(spark):
     val = get_data(spark, 'cf_validation', .01)
     test = get_data(spark, 'cf_test', .01)
 
-    train_len = len(train)
-    val_len = len(val)
-    test_len = len(test)
+    train_len = train.count()
+    val_len = val.count()
+    test_len = test.count()
 
     print('Begin indexing...')
-    
+
     for column in ['user', 'track']:
         indexer = StringIndexer(inputCol=f'{column}_id', outputCol=f'{column}_idx', handleInvalid='keep')
         indexed = indexer.fit(train)
@@ -26,17 +26,17 @@ def main(spark):
     print('Finished indexing...')
 
     # train = train.repartition(partitions, 'user_id').select(['user_idx', 'count', 'track_idx'])
-    # assert train_len == len(train)
+    # assert train_len == train.count()
     # train.write.parquet(path='processed_data/cf_train_idx.parquet', mode='overwrite')
     # train.unpersist()
     #
     # val = val.repartition(partitions, 'user_id').select(['user_idx', 'count', 'track_idx'])
-    # assert val_len == len(val)
+    # assert val_len == val.count()
     # val.write.parquet(path='processed_data/cf_validation_idx.parquet', mode='overwrite')
     # val.unpersist()
     #
     # test = test.repartition(partitions, 'user_id').select(['user_idx', 'count', 'track_idx'])
-    # assert test_len == len(test)
+    # assert test_len == test.count()
     # test.write.parquet(path='processed_data/cf_test_idx.parquet', mode='overwrite')
     # test.unpersist()
 
