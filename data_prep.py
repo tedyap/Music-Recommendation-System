3af78018acfbce3eb083ea73727aa6613576b0a2
Ted Yap <tyy231@nyu.edu>
2021-04-26 21:17:48 -0400

    unpersist

--- data_prep.py
+++ data_prep.py
@@ -9,6 +9,12 @@ def main(spark):
     val = get_data(spark, 'cf_validation', .01)
     test = get_data(spark, 'cf_test', .01)
 
+    train_len = len(train)
+    val_len = len(val)
+    test_len = len(test)
+
+    print('Begin indexing...')
+    
     for column in ['user', 'track']:
         indexer = StringIndexer(inputCol=f'{column}_id', outputCol=f'{column}_idx', handleInvalid='keep')
         indexed = indexer.fit(train)
@@ -17,14 +23,22 @@ def main(spark):
         test = indexed.transform(test)
         indexer.save(f'{column}_indexer')
 
-    train = train.repartition(partitions, 'user_id').select(['user_idx', 'count', 'track_idx'])
-    train.write.parquet(path='processed_data/cf_train_idx.parquet', mode='overwrite')
+    print('Finished indexing...')
 
-    val = val.repartition(partitions, 'user_id').select(['user_idx', 'count', 'track_idx'])
-    val.write.parquet(path='processed_data/cf_validation_idx.parquet', mode='overwrite')
-
-    test = test.repartition(partitions, 'user_id').select(['user_idx', 'count', 'track_idx'])
-    test.write.parquet(path='processed_data/cf_test_idx.parquet', mode='overwrite')
+    # train = train.repartition(partitions, 'user_id').select(['user_idx', 'count', 'track_idx'])
+    # assert train_len == len(train)
+    # train.write.parquet(path='processed_data/cf_train_idx.parquet', mode='overwrite')
+    # train.unpersist()
+    #
+    # val = val.repartition(partitions, 'user_id').select(['user_idx', 'count', 'track_idx'])
+    # assert val_len == len(val)
+    # val.write.parquet(path='processed_data/cf_validation_idx.parquet', mode='overwrite')
+    # val.unpersist()
+    #
+    # test = test.repartition(partitions, 'user_id').select(['user_idx', 'count', 'track_idx'])
+    # assert test_len == len(test)
+    # test.write.parquet(path='processed_data/cf_test_idx.parquet', mode='overwrite')
+    # test.unpersist()
 
 
 if __name__ == "__main__":
