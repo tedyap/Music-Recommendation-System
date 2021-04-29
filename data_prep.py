648a7e7c227904012298042c02b816aaf236a97e
akreiner <72363422+akreiner@users.noreply.github.com>
2021-04-28 20:47:57 -0400

    Update data_prep.py
    Added ALS function with regularization that returns RMSE

--- data_prep.py
+++ data_prep.py
@@ -6,7 +6,9 @@ Usage:
 
 from pyspark.sql import SparkSession
 from pyspark.ml.feature import StringIndexer
-
+from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.ml.recommendation import ALS
+from pyspark.sql import Row
 
 def main(spark):
     train = spark.read.parquet(f'hdfs:/user/bm106/pub/MSD/cf_train.parquet')
@@ -32,7 +34,15 @@ def main(spark):
     test = test.select(['user_idx', 'count', 'track_idx'])
     test.write.parquet(path='processed_data/cf_test_idx.parquet', mode='overwrite')
     test.unpersist()
-
+    
+def ALS(training,test,regularizationparam,userCol,itemCol,ratingCol):
+    #create and fit model, function args control type of fit
+    als = ALS(maxIter=5, regParam=regularizationparam, userCol=userCol, itemCol=itemCol, ratingCol=ratingCol, coldStartStrategy="drop")
+    model = als.fit(training)
+    predictions = model.transform(test)
+    evaluator = RegressionEvaluator(metricName="rmse", labelCol="rating", predictionCol="prediction")
+    rmse = evaluator.evaluate(predictions)
+    return RMSE
 
 if __name__ == "__main__":
     spark = SparkSession.builder.appName('data_prep').getOrCreate()
