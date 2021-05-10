f5b031dbbfac5a7c5edc661e7cb3b97c263bb42d
Max Needle <42817955+mneedle@users.noreply.github.com>
2021-05-10 19:11:33 -0400

    Update main_full.py

--- main_full.py
+++ main_full.py
@@ -16,7 +16,7 @@ from pyspark.sql import Row
 
 from pyspark.sql.functions import *
 from pyspark.sql.window import Window
-
+import pandas as pd
 
 def get_data(spark, file_name, frac_keep):
     # function to read and sample from dataset with constant seed across datasets
@@ -67,55 +67,79 @@ def main_full(spark,SUBSET_SIZE):
             predictions=predictions.withColumn("count_rank", rank().over(Window.partitionBy("user_idx").orderBy(desc("count"))))
             predictions=predictions.withColumn("prediction_rank", rank().over(Window.partitionBy("user_idx").orderBy(desc("prediction"))))
             predictions=predictions.filter(predictions.prediction_rank<=500)
+           
+   
+            true_dict = {} # create dictionary where key is user and value is tuple of count_rank list [0] and prediction_rank list [1]
+            p = predictions.toPandas()
+            for i in range(len(p)):
+                row = p.iloc[i,:] 
+                if row['user_idx'] in true_dict.keys():
+                    true_dict[row['user_idx']][0] += [row['count_rank']]
+                    true_dict[row['user_idx']][1] += [row['prediction_rank']]
+                else:
+                    true_dict[row['user_idx']] = [[row['count_rank']],[row['prediction_rank']]]
+             
+             list2 = []
+             for k in true_dict:
+                list2.append(true_dict[k])
+                
+            predictionAndLabels = sc.parallelize(list2)
+            metrics = RankingMetrics(predictionAndLabels)
+            MAP = metrics.meanAveragePrecision
             
-            
-            print(predictions)
-            
-            metrics_df=predictions.select(['prediction_rank','count_rank'])
-            metrics = RankingMetrics(metrics_df.rdd)
-            MAP=metrics.meanAveragePrecision
-            
-            #iterate over each row in predictions dataframe
-            #append ()
-            
-            evaluator = RegressionEvaluator(metricName="rmse", labelCol="count_rank", predictionCol="prediction_rank")
-            rmse = evaluator.evaluate(predictions)
-            
-            print('Current model: Rank:'+str(rnk)+', RegParam: '+str(reg)+', RMSE: '+str(rmse)+"MAP: "+str(MAP))
+            print(MAP)
 
-            #userRecs = model.recommendForAllUsers(500).show(5)
-
-            if count == 0:
-                best_model = model
-                best_map = MAP
-                stats = [rnk, reg, rmse,MAP]
-                count += 1
-            else:
-                if MAP < best_map:
-                    best_model = model
-                    best_map = MAP
-                    stats = [rnk, reg, rmse, MAP]
-    print('Best model: Rank: {}, RegParam: {}, RMSE: {}, MAP: {}'.format(*stats))
-    
-    rnk=stats[0]
-    reg=stats[1]
-    
-    als = ALS(rank=rnk, regParam=reg, userCol="user_idx", itemCol="track_idx", ratingCol="count", implicitPrefs=True, coldStartStrategy="drop")
-    model = als.fit(train)
-    predictions = model.transform(test)
-    
-    predictions=predictions.withColumn("count_rank", rank().over(Window.partitionBy("user_idx").orderBy(desc("count"))))
-    predictions=predictions.withColumn("prediction_rank", rank().over(Window.partitionBy("user_idx").orderBy(desc("prediction"))))
-    predictions=predictions.filter(predictions.prediction_rank<=500)
             
-    metrics_df=predictions.select(['prediction_rank','count_rank'])
-    metrics = RankingMetrics(metrics_df.rdd)
-    MAP=metrics.meanAveragePrecision
+            
+            
+            
+#             print(predictions)
+            
+#             metrics_df=predictions.select(['prediction_rank','count_rank'])
+#             metrics = RankingMetrics(metrics_df.rdd)
+#             MAP=metrics.meanAveragePrecision
+            
+#             #iterate over each row in predictions dataframe
+#             #append ()
+            
+#             evaluator = RegressionEvaluator(metricName="rmse", labelCol="count_rank", predictionCol="prediction_rank")
+#             rmse = evaluator.evaluate(predictions)
+            
+#             print('Current model: Rank:'+str(rnk)+', RegParam: '+str(reg)+', RMSE: '+str(rmse)+"MAP: "+str(MAP))
+
+#             #userRecs = model.recommendForAllUsers(500).show(5)
+
+#             if count == 0:
+#                 best_model = model
+#                 best_map = MAP
+#                 stats = [rnk, reg, rmse,MAP]
+#                 count += 1
+#             else:
+#                 if MAP < best_map:
+#                     best_model = model
+#                     best_map = MAP
+#                     stats = [rnk, reg, rmse, MAP]
+#     print('Best model: Rank: {}, RegParam: {}, RMSE: {}, MAP: {}'.format(*stats))
     
-    evaluator = RegressionEvaluator(metricName="rmse", labelCol="count_rank", predictionCol="prediction_rank")
-    rmse = evaluator.evaluate(predictions)
+#     rnk=stats[0]
+#     reg=stats[1]
     
-    print('Test Set: Rank:'+str(rnk)+', RegParam: '+str(reg)+', RMSE: '+str(rmse)+"MAP: "+str(MAP))
+#     als = ALS(rank=rnk, regParam=reg, userCol="user_idx", itemCol="track_idx", ratingCol="count", implicitPrefs=True, coldStartStrategy="drop")
+#     model = als.fit(train)
+#     predictions = model.transform(test)
+    
+#     predictions=predictions.withColumn("count_rank", rank().over(Window.partitionBy("user_idx").orderBy(desc("count"))))
+#     predictions=predictions.withColumn("prediction_rank", rank().over(Window.partitionBy("user_idx").orderBy(desc("prediction"))))
+#     predictions=predictions.filter(predictions.prediction_rank<=500)
+            
+#     metrics_df=predictions.select(['prediction_rank','count_rank'])
+#     metrics = RankingMetrics(metrics_df.rdd)
+#     MAP=metrics.meanAveragePrecision
+    
+#     evaluator = RegressionEvaluator(metricName="rmse", labelCol="count_rank", predictionCol="prediction_rank")
+#     rmse = evaluator.evaluate(predictions)
+    
+#     print('Test Set: Rank:'+str(rnk)+', RegParam: '+str(reg)+', RMSE: '+str(rmse)+"MAP: "+str(MAP))
     
 # Only enter this block if we're in main
 if __name__ == "__main__":
