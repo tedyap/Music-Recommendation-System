d6b560238bd5ae70936095298290f0848b404c7d
shaankhosla <skhoslajr@gmail.com>
2021-05-10 20:31:56 -0400

    write mode

--- bias.py
+++ bias.py
@@ -19,13 +19,13 @@ def main_full(SUBSET_SIZE):
     test = get_data('cf_test', SUBSET_SIZE)
     damps = [.25, .5, 1, 2, 5, 10, 15, 30, 50, 100, 150]
 
-    with open(f'/scratch/sk8520/temp/final-project-if_it_works_dont_touch_it/output.txt') as f:
+    with open(f'/scratch/sk8520/temp/final-project-if_it_works_dont_touch_it/output.txt', mode='w') as f:
         f.write(f'{damps}\n')
         f.write('file sizes: \n')
         f.write(f'train: {len(train)}')
         f.write(f'val: {len(val)}')
         f.write(f'test: {len(test)}')
-        
+
         for damp in damps:
             b = bias.Bias(items=True, users=True, damping=damp).fit(train)
             preds = [b.predict_for_user(user=row['user'], items=[row['item']]).values[0] for index, row in val.iterrows()]
