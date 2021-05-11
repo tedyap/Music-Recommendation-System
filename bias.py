9707ac675dc391576fb1f2aa761eab043b66a27c
shaankhosla <skhoslajr@gmail.com>
2021-05-10 20:06:57 -0400

    print metrics

--- bias.py
+++ bias.py
@@ -4,6 +4,7 @@
 
 from lenskit.algorithms import bias
 import pandas as pd
+from sklearn.metrics import average_precision_score
 
 def get_data(file_name, frac_keep):
     df = pd.read_parquet(f'/scratch/work/courses/DSGA1004-2021/MSD/{file_name}.parquet')
@@ -11,19 +12,19 @@ def get_data(file_name, frac_keep):
     df.rename(columns={'count':'rating', 'track_id':'item', 'user_id':'user'}, inplace=True)
     return df
 
-
 def main_full(SUBSET_SIZE):
     train = get_data('cf_train_new', SUBSET_SIZE)
     val = get_data('cf_validation', SUBSET_SIZE)
     test = get_data('cf_test', SUBSET_SIZE)
 
-    b = bias.Bias(items=True, users=True, damping=0).fit(train)
-    preds = [b.predict_for_user(user=row['user'], items=[row['item']]).values[0] for index, row in val.iterrows()]
-    print(len(preds))
-    for pred in preds[1:10]:
-        print(pred)
-        print(type(pred))
-        print()
+    print(train.shape())
+
+    for damp in [.5, 1, 2, 5]:
+        b = bias.Bias(items=True, users=True, damping=damp).fit(train)
+        preds = [b.predict_for_user(user=row['user'], items=[row['item']]).values[0] for index, row in val.iterrows()]
+        true_preds = val['rating'].tolist()
+        map = average_precision_score(y_true=true_preds, y_score=preds)
+        print(damp, map)
 
 if __name__ == "__main__":
 
