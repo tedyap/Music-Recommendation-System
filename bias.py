158bc6cf575a12b6498ae0b5c692ac09adb4fe06
shaankhosla <skhoslajr@gmail.com>
2021-05-10 20:31:07 -0400

    write to file

--- bias.py
+++ bias.py
@@ -17,14 +17,21 @@ def main_full(SUBSET_SIZE):
     train = get_data('cf_train_new', SUBSET_SIZE)
     val = get_data('cf_validation', SUBSET_SIZE)
     test = get_data('cf_test', SUBSET_SIZE)
+    damps = [.25, .5, 1, 2, 5, 10, 15, 30, 50, 100, 150]
 
-
-    for damp in [.25, .5, 1, 2, 5, 10, 15]:
-        b = bias.Bias(items=True, users=True, damping=damp).fit(train)
-        preds = [b.predict_for_user(user=row['user'], items=[row['item']]).values[0] for index, row in val.iterrows()]
-        true_preds = val['rating'].tolist()
-        rmse = mean_squared_error(y_true=true_preds, y_pred=preds)
-        print(damp, rmse)
+    with open(f'/scratch/sk8520/temp/final-project-if_it_works_dont_touch_it/output.txt') as f:
+        f.write(f'{damps}\n')
+        f.write('file sizes: \n')
+        f.write(f'train: {len(train)}')
+        f.write(f'val: {len(val)}')
+        f.write(f'test: {len(test)}')
+        
+        for damp in damps:
+            b = bias.Bias(items=True, users=True, damping=damp).fit(train)
+            preds = [b.predict_for_user(user=row['user'], items=[row['item']]).values[0] for index, row in val.iterrows()]
+            true_preds = val['rating'].tolist()
+            rmse = mean_squared_error(y_true=true_preds, y_pred=preds)
+            f.write(f'damping paramter: {damp} mean squared error: {rmse}\n')
 
 if __name__ == "__main__":
 
