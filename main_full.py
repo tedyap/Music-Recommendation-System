0b706b6b5d06afe0cbcbfe0c96c9337ad2245cf1
akreiner <72363422+akreiner@users.noreply.github.com>
2021-05-12 20:51:33 -0400

    Update main_full.py

--- main_full.py
+++ main_full.py
@@ -73,8 +73,8 @@ def main_full(spark,SUBSET_SIZE):
             Counter=0
             
             for user in userRecs.select("user_idx").distinct().show():
-                predicted=userRecs.filter(userRecs.user_idx == user).select("recommendations").show()[0]
-                actual=val.filter(userRecs.user_idx == user).select("track_idx").show()
+                predicted=userRecs.filter(userRecs.user_idx == user).select("recommendations").rdd.map(r => r(0)).collect()[0]
+                actual=val.filter(userRecs.user_idx == user).select("track_idx").rdd.map(r => r(0)).collect()
                 predictionAndLabels.append((predicted,actual))
                 
                 if Counter==0:
