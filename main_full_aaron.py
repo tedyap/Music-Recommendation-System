5eee454595e89fa8806c861a618ad8568b30b5c9
akreiner <72363422+akreiner@users.noreply.github.com>
2021-05-14 00:11:21 -0400

    Update main_full_aaron.py

--- main_full_aaron.py
+++ main_full_aaron.py
@@ -82,7 +82,7 @@ def main_full(spark,SUBSET_SIZE):
             userRecs = model.recommendForUserSubset(user_subset, 500)
             pred_label = userRecs.select('user_idx','recommendations.track_idx')
             pred_true_rdd = pred_label.join(F.broadcast(true_label), 'user_idx', 'inner').select('track_idx','true_item')
-            metrics = RankingMetrics(sc.parallelize(pred_true_rdd.rdd))
+            metrics = RankingMetrics(pred_true_rdd.rdd)
             map_ = metrics.meanAveragePrecision
             ndcg = metrics.ndcgAt(500)
             mpa = metrics.precisionAt(500)
